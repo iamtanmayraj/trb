864e8af5a3ca10137c76f6173f780c131c6fe464
iamtanmayraj <35463463+iamtanmayraj@users.noreply.github.com>
2018-01-25 23:20:28 +0530

    Create First_Repeted.c

---
+++ First_Repeted.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+	int i,j,n;
+	int ind,ele;
+	scanf("%d",&n);
+    long long int arr[n];
+		
+	for(i=0; i<n; i++)
+	{
+		scanf("%lld",&arr[i]);
+	}
+
+	ind=-1;
+	for(i=0; i<n; i++)
+	{
+		for(j=i+1; j<n; j++)
+		{
+			if(arr[i]==arr[j])
+			{
+				ele=arr[j];
+				ind=j;
+				break;
+			}				
+		}
+	}
+	if(ind!=-1)
+		printf("%d\n",ele);
+	else
+		printf("'unique'\n");
+
+	return 0;
+}
